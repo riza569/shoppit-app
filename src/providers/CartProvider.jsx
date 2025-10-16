b39073e1d7f2f7d453fa1abe0896943c0b1fabad
riza569 <rizamuhammed569@gmail.com>
2025-10-17 00:12:46 +0530

    CARTPROVIDER LINK FIXED

--- src/providers/CartProvider.jsx
+++ src/providers/CartProvider.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import CartContext from "../components/contexts/CartContext";
+import BASE_URL from "../lib/baseurl";
 const CartProvider = ({ children }) => {
   const [numCartItems, setNumCartItems] = useState(0);
   useEffect(() => {
     const cart_code = localStorage.getItem("cart_code");
     if (cart_code) {
-      fetch(`http://127.0.0.1:8000/get_cart_stat?cart_code=${cart_code}`)
+      fetch(`${BASE_URL}/get_cart_stat?cart_code=${cart_code}`)
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
