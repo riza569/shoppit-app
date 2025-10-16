ff3907471e554bf3c4a83442a95d3ad1c569de4c
riza569 <rizamuhammed569@gmail.com>
2025-10-17 00:16:41 +0530

    everything may be fixed

--- src/providers/CartProvider.jsx
+++ src/providers/CartProvider.jsx
@@ -6,7 +6,7 @@ const CartProvider = ({ children }) => {
   useEffect(() => {
     const cart_code = localStorage.getItem("cart_code");
     if (cart_code) {
-      fetch(`${BASE_URL}/get_cart_stat?cart_code=${cart_code}`)
+      fetch(`${BASE_URL}get_cart_stat?cart_code=${cart_code}`)
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
